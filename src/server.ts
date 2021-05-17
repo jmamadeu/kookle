7233c32b73c9d14ac945997916afdd60ed479a01
João Amadeu <jmamadeu2000@gmail.com>
2021-05-17 16:01:09 +0100

    :zap: chore: add jest

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
+import { celebrate, errors, Joi, Segments } from 'celebrate'
 import express from 'express'
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
 
 const app = express()
+const port = process.env.PORT || 3333
+
+const httpServer = createServer(app)
 
 app.get('/', (_, response) => {
   return response.json({
@@ -8,4 +14,28 @@ app.get('/', (_, response) => {
   })
 })
 
-app.listen(3333, () => console.log('Server is running'))
+app.get(
+  '/validation/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  (request, response) => {
+    console.log(request.params)
+
+    return response.json({
+      validator: true,
+    })
+  },
+)
+
+const io = new Server(httpServer)
+
+io.on('connection', (socket: Socket) => {
+  console.log(socket.id)
+})
+
+app.use(errors())
+
+httpServer.listen(port, () => console.log(`Server is running at port ${port}`))
