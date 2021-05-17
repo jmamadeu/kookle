05d2ee84920cd259b2a649990adaca37aae083a1
João Amadeu <jmamadeu2000@gmail.com>
2021-05-17 15:20:46 +0100

    :zap: chore: add editorconfig

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ const app = express()
 
 app.get('/', (_, response) => {
   return response.json({
-    message: 'spaceman bot is running'
+    message: 'spaceman bot is running',
   })
 })
 
-app.listen(3333, () => console.log("Server is running"))
+app.listen(3333, () => console.log('Server is running'))
